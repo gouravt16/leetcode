d3d345c7719c53118f89bade14ee1098af871487
gouravt16 <49191244+gouravt16@users.noreply.github.com>
2021-07-13 23:57:01 +0530

    Create Two sum.cpp

---
+++ Two sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        unordered_map<int, int> arr;
+        for(int i=0; i<nums.size(); ++i) {
+            
+            arr[i] = nums[i];                
+        }
+        for(int i=0; i<nums.size(); ++i) {
+            
+            if(arr.find(target - nums[i]) != arr.end()) {
+                ans.push_back(i);
+                cout << arr.find(target - nums[i]);
+                ans.push_back(5);
+                return ans;
+            }
+        }
+        return ans;
+    }
+};
