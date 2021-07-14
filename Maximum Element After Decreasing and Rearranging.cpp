afdd4f80164f17c6abc0ce6c80d047317e8b2800
gouravt16 <49191244+gouravt16@users.noreply.github.com>
2021-07-14 17:11:26 +0530

    Create Maximum Element After Decreasing and Rearranging.cpp

---
+++ Maximum Element After Decreasing and Rearranging.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/maximum-element-after-decreasing-and-rearranging/
+
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+        arr[0]=1;
+        int max = arr[0];
+        for(int i=1; i<arr.size(); ++i) {
+            
+            if(arr[i] != arr[i-1]) {
+                arr[i] = arr[i-1]+1;
+            }
+        }
+        return arr[arr.size()-1];
+    }
+};
