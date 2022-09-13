e8316e021ff00ee4cb782e8027322b7a33843437
gouravt16 <49191244+gouravt16@users.noreply.github.com>
2022-09-13 17:29:23 +0530

    Update _pascals-triangle.cpp

--- _pascals-triangle.cpp
+++ _pascals-triangle.cpp
@@ -18,3 +18,25 @@ public:
         return ans;
     }
 };
+
+Or
+
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+        int i,j;
+        vector<vector<int>> ans(numRows);   // determined number of rows of ans
+        for(i=0; i<numRows; ++i) {
+            ans[i].resize(i + 1);           // determined number of cols of ans
+            for(j=0; j<=i; ++j) {
+                if(j==0 || j==i) {
+                    ans[i][j]=1;
+                } else {
+                    ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
+                }
+            }
+        }
+        return ans;
+    }
+};
